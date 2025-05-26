35a1a3db1f04f41288d14ae8194f555bdd4c3e4b
seika <raityoseika@gmail.com>
2025-05-26 10:33:31 +0900

    Update renewquiz.js

--- renewquiz.js
+++ renewquiz.js
@@ -1,9 +1,9 @@
-//if(!sessionStorage.getItem('initialized')){
-	//localStorage.setItem("bit",0);
-	//localStorage.setItem("score",0);
-	//sessionStorage.setItem('initialized','true');
-//}
-//let bit = Number(localStorage.getItem("bit") ?? 0);
+if(!sessionStorage.getItem('initialized')){
+	localStorage.setItem("bit",0);
+	localStorage.setItem("score",0);
+	sessionStorage.setItem('initialized','true');
+}
+let bit = Number(localStorage.getItem("bit") ?? 0);
 const marker01 = document.querySelector('#marker01');
 const marker02 = document.querySelector('#marker02');
 const marker03 = document.querySelector('#marker03');
@@ -13,8 +13,8 @@ const ab2 = document.querySelector('#ab2');
 const ab3 = document.querySelector('#ab3');
 const ab4 = document.querySelector('#ab4');
 
- localStorage.setItem("bit",0);
- localStorage.setItem("score",0);
+// localStorage.setItem("bit",0);
+// localStorage.setItem("score",0);
 // localStorage.setItem("bit",0);
 
 function setButtonState(button, isActive) {
