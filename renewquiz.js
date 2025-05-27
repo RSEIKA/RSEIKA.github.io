afe0531609a263615a5a5c39818a15e110c6d572
seika <raityoseika@gmail.com>
2025-05-27 15:37:09 +0900

    Update renewquiz.js

--- renewquiz.js
+++ renewquiz.js
@@ -13,9 +13,9 @@ const ab2 = document.querySelector('#ab2');
 const ab3 = document.querySelector('#ab3');
 const ab4 = document.querySelector('#ab4');
 
- localStorage.setItem("bit",0);
- localStorage.setItem("score",0);
- localStorage.setItem("bit",0);
+// localStorage.setItem("bit",0);
+// localStorage.setItem("score",0);
+// localStorage.setItem("bit",0);
 
 function setButtonState(button, isActive) {
   if (isActive) {
